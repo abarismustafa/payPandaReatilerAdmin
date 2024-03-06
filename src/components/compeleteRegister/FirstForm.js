970cbf291a59a46bc3c2c6a3078e83633d40f86e
junaid <junaid.abaris@gmail.com>
2024-03-06 17:17:35 +0530

    solving prob

--- src/components/compeleteRegister/FirstForm.js
+++ src/components/compeleteRegister/FirstForm.js
@@ -1,9 +1,17 @@
-export const FirstForm = ({setState}) => {
+import { useState } from "react"
+import { panNumberVarify } from "../../api/login/Login"
+
+export const FirstForm = ({ setState }) => {
+    const [pannum, setpannum] = useState()
+    const submitPanNumber = async () => {
+        const res = await panNumberVarify({ pan: pannum, user_id: window.localStorage.getItem('userToken')})
+    }
     return <form>
         <div className="mb-3 pancars">
             <label for="exampleInputEmail1" className="form-label">Enter Pan Number</label>
             <div id="emailHelp" className="form-text">Enter your pan number. Pan number should belong to individual</div>
-            <input type="number" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <input type="text" onChange={(e) => { setpannum(e.target.value) }} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <button type="button" onClick={submitPanNumber} style={{ backgroundColor: '#2E3191', margin: "6px 0" }} disabled={!pannum ? true : false} className="btn btn-primary">Verify Pan</button>
         </div>
         <div className="mb-3 pancars">
             <label for="exampleInputPassword1" className="form-label">Enter Adhaar Number</label>
@@ -15,7 +23,7 @@ export const FirstForm = ({setState}) => {
             display: 'flex',
             justifyContent: 'end'
         }}>
-            <button type="submit" onClick={()=>setState((1))} style={{ backgroundColor: '#2E3191' }} className="btn btn-primary">Verify</button>
+            <button type="submit" onClick={() => setState((1))} style={{ backgroundColor: '#2E3191' }} disabled={true} className="btn btn-primary">Next</button>
         </div>
     </form >
 
